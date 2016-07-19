1265be08f4c8b33e8aad4168081e0b231b2c18c8
anoopgoudar <anoop.a.goudar@gmail.com>
2016-07-19 04:38:56 -0700

    schema for books

--- modules/books/server/models/book.server.model.js
+++ modules/books/server/models/book.server.model.js
@@ -10,9 +10,11 @@ var mongoose = require('mongoose'),
  * Book Schema
  */
 var BookSchema = new Schema({
-  created: {
-    type: Date,
-    default: Date.now
+  id: {
+    type: Number,
+    unique: true,
+    trim: true,
+    required: 'Id cannot be blank'
   },
   title: {
     type: String,
@@ -20,16 +22,125 @@ var BookSchema = new Schema({
     trim: true,
     required: 'Title cannot be blank'
   },
-  content: {
+  desc: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Description cannot be blank'
+  },
+  author: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Author name be blank'
+  },
+  publications: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Publications cannot be blank'
+  },
+  category: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Category cannot be blank'
+  },
+  state: {
+    type: Boolean,
+    default: false,
+    required: 'Title cannot be blank'
+  },
+  isbn: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'ISBN cannot be blank'
+  },
+  numberOfCopies: {
+    type: Number,
+    default: 1,
+    required: 'There should be a single copy atleast.'
+  },
+  condition: {
+    type: String,
+    default: 'new & good',
+    trim: true,
+    required: 'condition cannot be blank'
+  },
+  location: {
+    type: String,
+    default: '2nd floor',
+    trim: true,
+    required: 'Please tell the user where its available.'
+  },
+  contributedBy: {
+    type: String,
+    default: 'Admin',
+    trim: true,
+    required: 'Contiributor name is mandatory.'
+  },
+  isActive: {
+    type: Boolean,
+    default: true
+  },
+  reviewLink: {
     type: String,
     default: '',
     trim: true
   },
-  user: {
-    type: Schema.ObjectId,
-    ref: 'User'
+  imageUrl: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'ImageUrl cannot be blank'
   },
-  isbn: String
+  numberOfPages: {
+    type: Number,
+    default: ''
+  },
+  keywords: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'comma seperated keywords are expected.'
+  },
+  language: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  requestQueue: {
+    userId: {
+      type: Schema.Types.ObjectId,
+      default: '',
+      trim: true,
+      required: 'UserID cannot be blank'
+    },
+    queueNumber: {
+      type: Number
+    }
+  },
+  createdOn: {
+    type: Date,
+    default: Date.now
+  },
+  createdBy: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Please enter the name of the creator.'
+  },
+  updatedBy: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Please enter the name of the person who updated.'
+  },
+  updatedOn: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 mongoose.model('Book', BookSchema);
