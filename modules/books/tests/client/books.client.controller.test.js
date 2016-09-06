cbe52353da8439a5cb54f16dc17965390c771127
Rahul Raviprasad <rahul.raviprasad@gmail.com>
2016-09-06 05:53:20 -0700

    injecting controller for book test

--- modules/books/tests/client/books.client.controller.test.js
+++ modules/books/tests/client/books.client.controller.test.js
@@ -4,12 +4,27 @@
   function viewEditBookSuite() {
     var $scope;
     var BooksController;
+    var Authentication = {};
+    var bookResolve = {};
 
     beforeEach(module('books'));
     beforeEach(module(ApplicationConfiguration.applicationModuleName));
 
-    beforeEach(inject(function(_rootScope_, _$state_) {
-      $scope = _rootScope_.$new();
+    beforeEach(inject(function(_$rootScope_, _$state_, _$controller_, bookResolve, _$window_) {
+      $scope = _$rootScope_.$new();
+      BooksController = _$controller_('BooksController', {
+        $scope: $scope,
+        $state: _$state_,
+        Authentication: Authentication,
+        bookResolve: bookResolve,
+        $window: _$window_
+      });
+
     }));
+
+    it('should have set correct defaults', function() {
+      $scope.$digest();
+      expect(BooksController.error).toBe(null);
+    });
   }
 }());
