d471c2aba148e4f525f81506570e1aaf0e51d97c
Rahul Raviprasad <rahul.raviprasad@gmail.com>
2016-09-09 10:58:54 -0700

    added arrows to control the carousel

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -7,5 +7,17 @@
 
   function HomeController() {
     var vm = this;
+
+    vm.carouselIndex = 1;
+    vm.prev = prev;
+    vm.next = next;
+
+    // ------------------------------------------
+    function prev() {
+      vm.carouselIndex--;
+    }
+    function next() {
+      vm.carouselIndex++;
+    }
   }
 }());
