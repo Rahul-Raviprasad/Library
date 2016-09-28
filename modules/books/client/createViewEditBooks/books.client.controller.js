40539870df17455a4ef891814496ea728217115f
anoopgoudar <anoop.a.goudar@gmail.com>
2016-09-28 20:32:31 +0530

    having default image in create book form

--- modules/books/client/createViewEditBooks/books.client.controller.js
+++ modules/books/client/createViewEditBooks/books.client.controller.js
@@ -20,6 +20,7 @@
     vm.createReview = createReview;
     // vm.fileSelected = fileSelected;
     vm.editBook = editBook;
+    vm.imageURL = 'modules/books/client/img/default_book.jpeg';
     vm.uploadBookPicture = uploadBookPicture;
     vm.uploader = new FileUploader({
       url: 'api/book/picture',
