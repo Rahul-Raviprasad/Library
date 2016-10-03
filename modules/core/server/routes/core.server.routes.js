2a2dab04ff10376fad579d041c451d16726beedb
Rahul Raviprasad <rahul.raviprasad@gmail.com>
2016-10-03 05:58:06 -0700

    removed cyclic pointer in JSON stringify for req object, redirecting to pramati cas logout page

--- modules/core/server/routes/core.server.routes.js
+++ modules/core/server/routes/core.server.routes.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
   var core = require('../controllers/core.server.controller');
 
   // Define error pages
-  app.route('/server-error').get(core.renderServerError);
+  // app.route('/server-error').get(core.renderServerError);
 
 
   app.route('/check').get(function(req, res, next) {
@@ -19,26 +19,26 @@ module.exports = function (app) {
     res.send(req.session.cas.user);
   });
 
-  // Define application route
-  // cas.ssout('/routename'), cas.serviceValidate(), cas.authenticate()
-  app.route('/*').get(cas.ssout('/routename'), cas.serviceValidate(), cas.authenticate(), core.renderIndex);
-
-
-    //  for logout from other apps
-  app.route('/signout').post(cas.ssout('/routename'), function(req, res, next) { console.log('hi');});
-    // user logout
+  // user signs out from our app
   app.route('/signout').get(function(req, res, next) {
-    console.log('signing out : ' + JSON.stringfy(req));
     if (req.session.destroy) {
       req.session.destroy();
     } else {
       req.session = null;
     }
-    res.redirect('https://www.google.com/?custom_normal_logout=1');
+    // req.send('signed out');
+    res.redirect('https://corridor.pramati.com/?custom_normal_logout=1');
   });
+
+    //  for logout from other apps
+  app.route('/signout').post(cas.ssout('/signout'), function(req, res, next) { console.log('hi');});
 
   // Return a 404 for all undefined api, module or lib routes
   app.route('/:url(api|modules|lib)/*').get(core.renderNotFound);
+
+  // Define application route
+  // cas.ssout('/routename'), cas.serviceValidate(), cas.authenticate()
+  app.route('/*').get(cas.ssout('/routename'), cas.serviceValidate(), cas.authenticate(), core.renderIndex);
 
   // core.renderIndex
 
