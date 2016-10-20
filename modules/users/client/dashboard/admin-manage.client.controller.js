85ba3ac3c272a5b2a8aad88b2ffa540fa9e08fa7
anoopgoudar <anoop.a.goudar@gmail.com>
2016-10-20 19:11:57 +0530

    added authentication service

--- modules/users/client/dashboard/admin-manage.client.controller.js
+++ modules/users/client/dashboard/admin-manage.client.controller.js
@@ -5,10 +5,11 @@
     .module('core.admin')
     .controller('AdminManageCtrl', AdminManageCtrl);
 
-  AdminManageCtrl.inject = ['AdminServices'];
+  AdminManageCtrl.inject = ['AdminServices', 'Authentication'];
 
-  function AdminManageCtrl(AdminServices) {
+  function AdminManageCtrl(AdminServices, Authentication) {
     var vm = this;
+    vm.authentication = Authentication;
     // vm.admins = ['a', 'b', 'c', 'd'];
     AdminServices.getAllAdmins().then(successHandler);
 
