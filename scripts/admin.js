a38ffde1ebffad9d3db9456406e9b18583f6af3e
Rahul Raviprasad <rahul.raviprasad@gmail.com>
2016-11-21 15:21:15 +0530

    added script to make me admin

---
+++ scripts/admin.js
@@ -0,0 +1,2 @@
+use mean-dev
+db.users.insert({"displayName":"Rahul Raviprasad","provider":"local","username":"Rahul","roles":["user", "admin"],"email":"rahul.raviprasad@imaginea.com","lastName":"Raviprasad","firstName":"Rahul"})
